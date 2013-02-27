ee80d1ff76603712aea9bea3ee86dbc6c3dcf078
Aki Balogh <akibalogh@gmail.com>
2013-02-27 13:35:07 -0600

    added StackOverflow query utility

--- utils/dump_user_info.py
+++ utils/dump_user_info.py
@@ -1,7 +1,7 @@
 import sys
 import pymongo
 from pymongo import MongoClient
-
+import hashlib
 
 def print_user_keywords(users):
 	# make a list of keywords users interested in
@@ -16,5 +16,25 @@ if __name__ == "__main__":
 	connection = MongoClient()
 	db = connection.db
 	users = db.users.find()
-	print "User count: ", users.count()
-	print_user_keywords(users)
+	so_users = db.so_users
+	# print "User count: ", users.count()
+	# print_user_keywords(users)
+
+	found_counter = 0
+	not_found_counter = 0
+
+	for user in users:
+		email_hash = hashlib.md5(user['e']).hexdigest()
+		#print "e: " + user['e'] + " hash: " + email_hash
+
+		found_user = None
+		found_user = so_users.find_one( { "eh" : email_hash } )
+		if (found_user is not None):
+			print "Found user! e: " + user['e'] + " hash: " + str(found_user['eh'])
+			found_counter += 1
+		else:
+			#print "Did not find user e: " + user['e']
+			not_found_counter += 1
+
+	found_percent = float(found_counter) / float(not_found_counter) * 100
+	print "Found: " + str(found_counter) + "/" + str(found_counter + not_found_counter) + " (" + "{0:.0f}".format(found_percent) + "%)"
