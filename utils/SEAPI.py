852ab0411d99cc28dfa2ec68cb6510e69f4d69f0
Aki Balogh <akibalogh@gmail.com>
2013-03-04 23:13:49 -0600

    modified SEAPI to insert into Mongo

--- utils/SEAPI.py
+++ utils/SEAPI.py
@@ -1,12 +1,14 @@
 # Note: this library was forked from https://github.com/stared/se-api-py
 
 import requests
+import pymongo
+from pymongo import MongoClient
 try:
     import simplejson as json
 except:
     import json
 from time import time, sleep
-
+import sys
 
 class SEAPI():
     def __init__(self, **kwargs):
@@ -28,23 +30,6 @@ class SEAPI():
         self.last_call = ["", {}]
         self.last_status = {}
         self.last_response_times = []
-
-    # def _replace_placeholders(self, command, **kwargs):
-    #     """Parses API command to find placeholders
-    #        for elements '{foo}' or lists '{foos}',
-    #        replacing them with kwargs foo=number, or foos=list;
-    #        EXAMPLE:
-    #        >>> seapi._replace_placeholders("users/{ids}/comments/{toid}",
-    #                                  ids = [1, 2], toid = 5)
-    #        "users/1;2/comments/5"  """
-    #     parts = command.split("/")
-    #     for i, part in enumerate(parts):
-    #         if part.startswith("{"):
-    #             if part.endswith("s}"):
-    #                 parts[i] = ";".join(map(str, kwargs[part[1:-1]]))
-    #             elif part.endswith("}"):
-    #                 parts[i] = str(kwargs[part[1:-1]])
-    #     return "/".join(parts)
 
     def _find_placeholders(self, raw_command, **kwargs):
         parts = raw_command.split("/")
@@ -74,6 +59,39 @@ class SEAPI():
         [[2, 3, 5], [7, 11, 13], [17, 19]]"""
         return [li[i:(i + k)] for i in xrange(0, len(li), k)]
 
+    def clean_return(self, fetch_return, so_users):
+	return_list = []
+
+	for user in fetch_return:
+		# Ignore users already in MongoDB
+		if (so_users.find_one({"sid": user['account_id']}) == None):
+			try:
+        	        	email_hash = user['profile_image'][31:63]
+	        	        if(len(email_hash) < 32):
+                       			email_hash = ""
+		        except KeyError:
+        		        email_hash = ""
+
+		        try:
+        		        location = user['location']
+	        	except KeyError:
+        	        	location = ""
+
+		        try:
+        		        web_url = user['website_url']
+	       		except KeyError:
+        		        web_url = ""
+
+			single_user = {  
+				"sid": user['account_id'],
+				"dn": user['display_name'],
+				"eh": email_hash,
+				"l": location,
+				"w": web_url }
+			return_list.append(single_user)
+	return return_list
+
+
     def fetch_one(self, command, subcommand=False, parse_curly_parameters=True, **kwargs):
         """Returns one page of results for a given command;
         EXAMPLE:
@@ -101,19 +119,19 @@ class SEAPI():
         self.last_response_times.append(time() - t0)
 
         data = json.loads(r.content)
-
         self.last_call = [url, parameters]
         self.last_status = dict([(k, data[k]) for k in data if k != 'items'])
 
         return data['items']
 
-    def fetch(self, command, starting_page=1, page_limit=2000, print_progress=True, min_delay=0.05, **kwargs):
+    def fetch(self, command, mongo_db, starting_page=1, page_limit=2000, print_progress=True, min_delay=0.05, **kwargs):
         """Returns all pages (withing the limit) of results for a given command;
         automatically splits lists for {ids} in appropriate chunks
         EXAMPLE -> check for fetch_one (without 'page' parameter!)
         NOTE: Here is a lot of room for improvements and additional features, e.g.:
         - gevent for concurrency to get optimal rate
         - dealing with 'Violation of backoff parameter' """
+	so_users = mongo_db.so_users
         res = []
         self.last_response_times = []
         parts, curly_params, curly_list_params = self._find_placeholders(command, **kwargs)
@@ -130,13 +148,16 @@ class SEAPI():
             pieces = self._chunks(curly_list, parameters['pagesize'])
             for piece in pieces:
                 command = self._combine_placeholders(parts, curly_params, {curly_name: piece})
-                res += self.fetch_one(command, subcommand=True, parse_curly_parameters=False, **parameters)
+                fetch_return = self.fetch_one(command, subcommand=True, parse_curly_parameters=False, **parameters)
+		fetch_return = self.clean_return(fetch_return, so_users)
+		if (len(fetch_return) > 0):
+			ret = so_users.insert(fetch_return)
+   		res += fetch_return
                 # ^ maybe also with try/except/once_again?
                 if self.last_response_times[-1] < min_delay:
                         sleep(min_delay - self.last_response_times[-1])
                         if print_progress:
-                            #print "waited %.2f" % (min_delay - self.last_response_times[-1])
-                            print "Waited {0:.2} sec. API calls left: {1}".format(min_delay - self.last_response_times[-1], self.last_status['quota_remaining'])
+                            print "Waited {0:.2} sec. API calls left: {1}. Total users: {2}".format(min_delay - self.last_response_times[-1], self.last_status['quota_remaining'], so_users.count())
         else:
             command = self._combine_placeholders(parts, curly_params, {})
             if print_progress:
@@ -145,22 +166,30 @@ class SEAPI():
                 if print_progress:
                     print i,
                 try:
-                    res += self.fetch_one(command, subcommand=True, parse_curly_parameters=False, page=i, **parameters)
+                    fetch_return = self.fetch_one(command, subcommand=True, parse_curly_parameters=False, page=i, **parameters)
+		    fetch_return = self.clean_return(fetch_return, so_users)
+		    if (len(fetch_return) > 0):
+		    	ret = so_users.insert(fetch_return)
+		    res += fetch_return
                 except:
+                    print sys.exc_info()
                     print self.last_status
-                    print "waiting 60s and trying once more"  # mainly for 'Violation of backoff parameter'
+		    print "waiting 60s and trying once more"  # mainly for 'Violation of backoff parameter'
                     sleep(60.)
-                    res += self.fetch_one(command, subcommand=True, parse_curly_parameters=False, page=i, **parameters)
+		    fetch_return = self.fetch_one(command, subcommand=True, parse_curly_parameters=False, page=i, **parameters)
+		    fetch_return = self.clean_return(fetch_return, so_users)
+		    if (len(fetch_return) > 0):
+		    	ret = so_users.insert(fetch_return)
+		    res += fetch_return
                 if not self.last_status['has_more']:
                     break
                 if self.last_response_times[-1] < min_delay:
                     sleep(min_delay - self.last_response_times[-1])
                     if print_progress:
-                        #print "waited %.2f" % (min_delay - self.last_response_times[-1])
-                        print "Waited {0:.2} sec. API calls left: {1}".format(min_delay - self.last_response_times[-1], self.last_status['quota_remaining'])
-
+                        print "Waited {0:.2} sec. API calls left: {1}. Total users: {2}".format(min_delay - self.last_response_times[-1], self.last_status['quota_remaining'], so_users.count())
         return res
 
     def status_of(self, command, **kwargs):
         self.fetch_one(command, **kwargs)
         return self.last_status
+
