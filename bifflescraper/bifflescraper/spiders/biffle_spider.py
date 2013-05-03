6a0d2a7edc3b8190f1a4d7277086c1436495c927
Aki Balogh <akibalogh@gmail.com>
2013-05-02 23:09:13 -0500

    added: crawling multiple layers; output keyword frequency to file; bloom filter to check whether URL has been crawled

--- bifflescraper/bifflescraper/spiders/biffle_spider.py
+++ bifflescraper/bifflescraper/spiders/biffle_spider.py
@@ -5,6 +5,8 @@ from scrapy.item import Item
 from scrapy.http import HtmlResponse
 from scrapy.http.request import Request
 from scrapy import log
+from scrapy import signals
+from scrapy.xlib.pydispatch import dispatcher
 from bifflescraper.items import BiffleItem
 from boilerpipe.extract import Extractor
 from pybloom import BloomFilter
@@ -12,16 +14,24 @@ from pybloom import BloomFilter
 from datetime import datetime
 import urlparse
 import re
+import os
+from os.path import isfile, join
+import codecs
 
 class BiffleSpider(CrawlSpider):
 	# See: http://stackoverflow.com/questions/8320730/scrapy-log-handler
 	name = 'bifflescraper'
+	
+	# Use a Bloom Filter to track what URLs have already been visited
+	# See: https://github.com/jaybaird/python-bloomfilter
+	global url_bf
+	url_bf = BloomFilter(capacity=100000, error_rate=0.01)
 
-	allowed_domains = ['starnewsonline.com']
-	start_urls = ['http://www.starnewsonline.com/']
+	#allowed_domains = ['starnewsonline.com']
+	#start_urls = ['http://www.starnewsonline.com/']
 
-	#allowed_domains = ['www.techcentral.ie']
-	#start_urls = ['http://www.techcentral.ie']
+	allowed_domains = ['www.techcentral.ie']
+	start_urls = ['http://www.techcentral.ie']
 
 	# Get allowed domains
 	#domain_list_path = '/root/semanticgen/domains'
@@ -40,25 +50,70 @@ class BiffleSpider(CrawlSpider):
 
 	# Start the logger
 	log.start()
+	
+	def __init__(self, *a, **kw):
+        	super(BiffleSpider, self).__init__(*a, **kw)
+		dispatcher.connect(self.reduce_at_finish, signals.spider_closed)
 
-	def parse_page(self, response, recursion=True):
+	def map_keyword_count(self, found_list):
+		filename = '/root/biffle-prototype/bifflescraper/keyword_map'
+		with open(filename, 'a') as f:
+			for keyword in found_list:
+				f.write(keyword + ' 1\n')
+
+	def reduce_at_finish(self):
+		keywords = {}
+		filename = '/root/biffle-prototype/bifflescraper/keyword_map'		
+
+		# Open the workfile and parse domains into a data structure
+		# TODO: Is utf-8 needed?
+		infile = codecs.open(filename, 'r', encoding='utf-8')
+
+		try:
+			for line in infile.readlines():
+				(keyword, value) = line.split(' ')
+
+				if (keywords.has_key(keyword)):
+					keywords[keyword] = keywords[keyword] + int(value)
+				else:
+					keywords[keyword] = int(value)
+		except ValueError:
+			log.msg("Parse error at %s" % line, level=log.ERROR)
+
+		infile.close()
+
+		# Delete the original file
+		if (os.path.isfile(filename)):
+			os.unlink(filename)
+		else:
+			raise Exception("ERROR: Can't find keyword file for deletion")
+
+		# Write the data structure to a file
+		outfile = codecs.open(filename, 'a+', encoding='utf-8')
+		for key, value in keywords.items():
+				outfile.write(key + ' ' + str(value) + '\n')
+		outfile.close()
+
+
+	#                              Set search depth here
+	def parse_page(self, response, depth=2):
 		if not isinstance(response, HtmlResponse):
 		    log.msg('Not an HTML file: %s' % response.url, level=log.WARNING)
 		    return
 
 		log.msg('Response from: %s' % response.url, level=log.INFO)
+		url_bf.add(response.url)
 	
-		# TODO: Shift this into class __init__ code
+		# TODO: Shift this into class __init__ code?
 		keywordfilepath = '/root/biffle-prototype/utils/1gram-keyword-dump'
-		# See: https://github.com/jaybaird/python-bloomfilter
-		#keywordbf = BloomFilter(capacity=100000, error_rate=0.01)
 		keywordlist = []
 		with open(keywordfilepath) as f:
 		        for keyword in f:
-		                #keywordbf.add(keyword.rstrip())
 				# TODO: Figure out a better way to implement. Here, filter 2-letter keywords.
 				if (len(keyword) > 2):
 					keywordlist.append(keyword.rstrip('\n'))
+
+		# TODO: Extract page title
 	
 		extractor = Extractor(extractor='ArticleExtractor', html=response.body_as_unicode())
 		cleaned_text = extractor.getText()
@@ -66,39 +121,34 @@ class BiffleSpider(CrawlSpider):
 		# Eliminate duplicates
 		keywordset = set(keywordlist)
 
-		foundlist = []
+		found_list = []
 		for keyword in keywordset: # TODO: Is there a more efficient way to do this?
 			# Look at word boundaries to match entire words only
 			if (re.search(r'\b' + re.escape(keyword) + r'\b', cleaned_text)):
-				foundlist.append(keyword)
+				found_list.append(keyword)
 
 		# Parse this page		
 		item = BiffleItem()
-		if (len(foundlist) > 0):
+		if (len(found_list) > 0):
 			item['url'] = response.url
 			item['body'] = cleaned_text
-			item['keywords'] = ', '.join(foundlist)
+			item['keywords'] = ', '.join(found_list)
 			item['process_date'] = datetime.today()
-			log.msg("Keyword(s) found: %s" % ', '.join(foundlist), level=log.INFO)
+			log.msg("Keyword(s) found: %s" % ', '.join(found_list), level=log.INFO)
+			self.map_keyword_count(found_list)
 			yield item
 
-		if (recursion is True):	
+		if (depth > 0):	
 			# Find the next requests and yield those
 			hxs = HtmlXPathSelector(response)
 			links = hxs.select('//a/@href').extract()
 			log.msg('Links on page: %s' % len(links), level=log.INFO)
 			for l in links:
 				l = urlparse.urljoin(response.url, l)
-				#log.msg('link: %s' % l, level=log.INFO)
+				if (l not in url_bf):
+					url_bf.add(l)
+					#log.msg('Found link: %s | From URL: %s' % (l, response.url), level=log.INFO)
+					# Decrement depth for next layer of links
+					callback = lambda response: self.parse_page(response, depth=(depth - 1))
+					yield Request(l, callback=callback)
 
-				# Recursion is set to false so further links aren't followed
-				# See: https://groups.google.com/forum/?fromgroups=#!topic/scrapy-users/zwrQYp3mguk
-				callback = lambda response: self.parse_page(response, recursion=False)
-				yield Request(l, callback=callback)
-
-
-	def save_article_file(self, response):
-		pass
-		#path = self.get_path(response.url)
-		#with open(path, 'wb') as f:
-		#	f.write(response.body)
